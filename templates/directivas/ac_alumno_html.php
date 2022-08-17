3c2e592eaee02786dac25aee4293c2fb38273f68
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-17 12:35:44 -0500

    v0.11.2 Se integro CRUD ac_alumno

---
+++ templates/directivas/ac_alumno_html.php
@@ -0,0 +1,14 @@
+<?php
+namespace html;
+
+use gamboamartin\academico\controllers\controlador_ac_centro_educativo;
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
+
+
+class ac_alumno_html extends html_controler {
+
+
+}
