65ba3330b9c4ca8edb7489f4b8de2a5197d10cd0
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-17 10:09:42 -0500

    v0.7.1 Se integro a entidad plan_estudio relacion con ac_nivel

--- orm/ac_plan_estudio.php
+++ orm/ac_plan_estudio.php
@@ -6,7 +6,7 @@ use PDO;
 class ac_plan_estudio extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'ac_nivel');
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
