d2acd22c83b5de69b03ca3132d1a398fb1863d4d
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-17 10:42:17 -0500

    v0.10.1 Se integra CRUD ac_turno

---
+++ controllers/controlador_ac_turno.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\academico\controllers;
+
+
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template_1\html;
+use html\ac_centro_educativo_html;
+use html\ac_nivel_html;
+use html\ac_plan_estudio_html;
+use html\ac_turno_html;
+use models\ac_centro_educativo;
+use models\ac_nivel;
+use models\ac_plan_estudio;
+use models\ac_turno;
+use PDO;
+use stdClass;
+
+class controlador_ac_turno extends system {
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+
+        $modelo = new ac_turno(link: $link);
+        $html_ = new ac_turno_html($html);
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Nivel';
+    }
+
+
+}
