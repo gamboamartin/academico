f1194ad1cdef98827cfcd0b2ddaa9a7c198d6458
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-17 12:12:38 -0500

    v0.10.2 FIX se edito relacion ac_nivel

--- orm/ac_plan_estudio.php
+++ orm/ac_plan_estudio.php
@@ -6,7 +6,7 @@ use PDO;
 class ac_plan_estudio extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false,'ac_nivel');
+        $columnas = array($tabla=>false,'ac_nivel'=>$tabla);
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
