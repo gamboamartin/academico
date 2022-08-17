5a46200a81db2168720a86f6f3e633f2e5b27fd9
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-17 13:05:54 -0500

    v0.12.2 Se integro relaciones de entidad ac_alumno

--- orm/ac_alumno.php
+++ orm/ac_alumno.php
@@ -6,7 +6,8 @@ use PDO;
 class ac_alumno extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'ac_estado_alumno'=>$tabla,'ac_turno'=>$tabla,'dp_calle_pertenece'=>$tabla,
+            'dp_estado'=>$tabla,'adm_estado_civil'=>$tabla,'adm_genero'=>$tabla, 'adm_idioma'=>$tabla);
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
