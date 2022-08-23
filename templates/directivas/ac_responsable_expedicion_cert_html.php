f9c821ee40f780b51128a10f460d3fd41d821f3f
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-23 10:24:47 -0500

    v0.45.5 Se integro CRUD de entidad responsable_expedicion_cert

---
+++ templates/directivas/ac_responsable_expedicion_cert_html.php
@@ -0,0 +1,31 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
+use models\ac_responsable_expedicion_cert;
+use PDO;
+use stdClass;
+
+
+class ac_responsable_expedicion_cert_html extends html_controler {
+
+    public function select_ac_responsable_expedicion_cert_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+
+        $modelo = new ac_responsable_expedicion_cert($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo, label: 'Responsable Expedicion');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+}
