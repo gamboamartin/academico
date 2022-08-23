acb451acb0daa3293296742712836edf8924aa15
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-23 10:36:34 -0500

    v0.46.5 Se integra relacion responsable_expedicion a entidad univeridad

--- orm/ac_universidad.php
+++ orm/ac_universidad.php
@@ -6,7 +6,7 @@ use PDO;
 class ac_universidad extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'ac_responsable_expedicion_cert' => $tabla);
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
