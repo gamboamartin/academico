49270cf21e00505d0606e7c280414652c603fef4
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-23 13:11:57 -0500

    v0.58.6 Se integra campo id vistas modifica

--- templates/inputs/_base/ac_rvoe/form.php
+++ templates/inputs/_base/ac_rvoe/form.php
@@ -1,5 +1,8 @@
 <?php /** @var gamboamartin\academico\controllers\controlador_ac_plan_estudio $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
+<?php if(isset($controlador->inputs->id)){ ?>
+<?php echo $controlador->inputs->id; ?>
+<?php } ?>
 <?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->codigo_bis; ?>
 <?php echo $controlador->inputs->descripcion_select; ?>
