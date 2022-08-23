8500d84523d8c91300f563d3019af7e37add6416
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-23 09:20:31 -0500

    v0.41.4 Se integra CRUD de entidad tipo_asignatura

---
+++ templates/directivas/ac_tipo_asignatura_html.php
@@ -0,0 +1,33 @@
+<?php
+namespace html;
+
+use gamboamartin\academico\controllers\controlador_ac_centro_educativo;
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
+use models\ac_nivel;
+use models\ac_tipo_asignatura;
+use PDO;
+use stdClass;
+
+
+class ac_tipo_asignatura_html extends html_controler {
+
+    public function select_ac_tipo_asignatura_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+
+        $modelo = new ac_tipo_asignatura($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo, label: 'Nivel');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+}
