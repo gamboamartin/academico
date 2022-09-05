6013caedd115e2e5e61a8a572e8f75822bff7423
javier <javier.flores@ivitec.com.mx>
2022-09-05 10:49:39 -0500

    v0.71.6 se elimino codigo y codigo bis de la vista modifica en ac materia

--- templates/inputs/_base/ac_materia/form.php
+++ templates/inputs/_base/ac_materia/form.php
@@ -3,8 +3,6 @@
 <?php if(isset($controlador->inputs->id)){ ?>
     <?php echo $controlador->inputs->id; ?>
 <?php } ?>
-<?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->codigo_bis; ?>
 <?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->id_asignatura; ?>
 <?php echo $controlador->inputs->no_creditos; ?>
