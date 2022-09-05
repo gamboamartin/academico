d4dc357615eed2bca10189631d64024feb59636a
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-05 14:34:44 -0500

    v0.76.6 Se integro validadicion regex telefono y correo front modifica

--- js/ac_alumno/modifica.js
+++ js/ac_alumno/modifica.js
@@ -10,3 +10,74 @@ let direcciones_js = url+'js/direcciones.js';
 document.write('<script src="'+direcciones_js+'"></script>');
 
 let session_id = getParameterByName('session_id');
+
+$( document ).ready(function() {
+    let telefono_txt_fijo = $("input[name=telefono_fijo]");
+    let telefono_txt_movil = $("input[name=telefono_movil]");
+    let telefono_error_fijo = $(".label-error-fijo");
+    let telefono_error_movil = $(".label-error-movil");
+    let telefono = '';
+    let telefono_regex = new RegExp('[1-9][0-9]{9}');
+
+    telefono_error_fijo.hide();
+    telefono_txt_fijo.keyup(function () {
+        telefono = $(this).val();
+        let valido = false;
+        let regex_val = telefono_regex.test(telefono);
+        let n_car = telefono.length;
+
+        if(n_car<=10 && regex_val){
+            valido = true;
+        }
+
+        if(!valido){
+            telefono_error_fijo.show();
+        } else {
+            telefono_error_fijo.hide();
+        }
+
+    });
+
+    telefono_error_movil.hide();
+    telefono_txt_movil.keyup(function () {
+        telefono = $(this).val();
+        let valido = false;
+        let regex_val = telefono_regex.test(telefono);
+        let n_car = telefono.length;
+
+        if(n_car<=10 && regex_val){
+            valido = true;
+        }
+
+        if(!valido){
+            telefono_error_movil.show();
+        } else {
+            telefono_error_movil.hide();
+        }
+
+    });
+
+    let txt_correo = $("input[name=correo]");
+    let correo_regex = new RegExp('[a-z0-9!#$%&\'*+=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+    let correo_error = $(".label-error-correo");
+
+    correo_error.hide();
+    txt_correo.change(function () {
+        let correo = $(this).val();
+        let valido = false;
+        let regex_val = correo_regex.test(correo);
+        let n_car = correo.length;
+
+        if(n_car > 0 && regex_val){
+            valido = true;
+        }
+
+        if(!valido){
+            correo_error.show();
+        } else {
+            correo_error.hide();
+        }
+    });
+
+
+});
