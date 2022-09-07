1ad9deec4a86fdf77d91c97e8b2876eb978aab55
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-07 11:31:38 -0500

    v0.91.6 Se ajusta lista vista asigna_plantel

--- views/ac_alumno/asigna_plantel.php
+++ views/ac_alumno/asigna_plantel.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
-                    <form method="post" action="<?php echo $controlador->link_org_sucursal_alta_bd; ?>" class="form-additional">
+                    <form method="post" action="<?php echo $controlador->link_ac_alumno_pertenece_alta_bd; ?>" class="form-additional">
                         <?php include (new views())->ruta_templates."head/title.php"; ?>
                         <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
                         <?php include (new views())->ruta_templates."mensajes.php"; ?>
@@ -38,42 +38,32 @@
                     <div class="">
                         <table class="table table-striped footable-sort" data-sorting="true">
                             <th>Id</th>
-                            <th>Codigo</th>
-                            <th>Tipo</th>
-                            <th>Descripcion</th>
-                            <th>Fecha Inicio</th>
-                            <th>Calle</th>
-                            <th>Ext</th>
-                            <th>Int</th>
-                            <th>CP</th>
-                            <th>Mun</th>
-                            <th>Edo</th>
-                            <th>Tel</th>
-                            <th>Serie</th>
+                            <th>Matricula Alumno</th>
+                            <th>Nombre Alumno</th>
+                            <th>Apellido Paterno</th>
+                            <th>Apellido Materno</th>
+                            <th>Fecha Ingreso</th>
+                            <th>Direccion</th>
+
                             <th>Ver</th>
                             <th>Modifica</th>
                             <th>Elimina</th>
 
                             <tbody>
-                            <?php foreach ($controlador->sucursales->registros as $sucursal){
+                            <?php foreach ($controlador->alumnos->registros as $alumno){
                                 ?>
                             <tr>
-                                <td><?php echo $sucursal['org_sucursal_id']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_codigo']; ?></td>
-                                <td><?php echo $sucursal['org_tipo_sucursal_descripcion']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_descripcion']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_fecha_inicio_operaciones']; ?></td>
-                                <td><?php echo $sucursal['dp_calle_descripcion']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_exterior']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_interior']; ?></td>
-                                <td><?php echo $sucursal['dp_cp_descripcion']; ?></td>
-                                <td><?php echo $sucursal['dp_municipio_descripcion']; ?></td>
-                                <td><?php echo $sucursal['dp_estado_descripcion']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_telefono_1']; ?></td>
-                                <td><?php echo $sucursal['org_sucursal_serie']; ?></td>
-                                <td><?php echo $sucursal['link_ve']; ?></td>
-                                <td><?php echo $sucursal['link_modifica']; ?></td>
-                                <td><?php echo $sucursal['link_elimina']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_pertenece_id']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_matricula']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_nombre']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_apellido_paterno']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_apellido_materno']; ?></td>
+                                <td><?php echo $alumno['ac_alumno_pertenece_fecha_ingreso']; ?></td>
+                                <td><?php echo $alumno['direccion']; ?></td>
+
+                                <td><?php echo $alumno['link_ve']; ?></td>
+                                <td><?php echo $alumno['link_modifica']; ?></td>
+                                <td><?php echo $alumno['link_elimina']; ?></td>
 
                             </tr>
                             <?php } ?>
