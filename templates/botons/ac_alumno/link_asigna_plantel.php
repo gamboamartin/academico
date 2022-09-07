934cdeaed030f57016d3ab0d5188343ae6483869
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-07 09:34:41 -0500

    v0.87.6 Se integra estilo a boton asigna_plantel

--- templates/botons/ac_alumno/link_asigna_plantel.php
+++ templates/botons/ac_alumno/link_asigna_plantel.php
@@ -1,4 +1,4 @@
 <?php /** @var stdClass $row  Registro */ ?>
 <a href="<?php echo $row->link_asigna_plantel; ?>" class="btn btn-<?php echo $row->link_asigna_plantel_style; ?>"><i class="icon-edit"></i>
-    Sucursales
+    Asigna Plantel
 </a>
