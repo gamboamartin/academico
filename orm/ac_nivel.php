fc20c5aa71e6ad32625a68ebea305c0dc8162bc7
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-01-03 14:27:25 -0600

    v0.130.12 Integracion de namespace

--- orm/ac_nivel.php
+++ orm/ac_nivel.php
@@ -11,5 +11,7 @@ class ac_nivel extends modelo{
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas);
+        
+        $this->NAMESPACE = __NAMESPACE__;
     }
 }
