64b46b90b81a9375985b4321acbf37e202f327d1
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-01-06 12:42:02 -0600

    v0.135.12 Se integra maquetacion alta

--- templates/inputs/_base/ac_rvoe/form.php
+++ templates/inputs/_base/ac_rvoe/form.php
@@ -5,7 +5,7 @@
 <?php } ?>
 <?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->codigo_bis; ?>
-<?php echo $controlador->inputs->descripcion_select; ?>
+<?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->alias; ?>
 <?php echo $controlador->inputs->numero; ?>
 <?php echo $controlador->inputs->fecha_expedicion; ?>
